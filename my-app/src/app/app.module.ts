c13b3867313321d3fbb414464ffede87c1c1e14f
Everton Roberto <everton_roliveira@outlook.com>
2017-09-22 21:43:27 -0300

    header quase finalizado

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ShowcaseComponent } from './home/showcase/showcase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductComponent,
+    ShowcaseComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
