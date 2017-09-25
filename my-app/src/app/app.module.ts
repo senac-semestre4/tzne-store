68a425705c3c8631ac376d6bdf9165624824fa84
Everton Roberto <everton_roliveira@outlook.com>
2017-09-24 21:04:54 -0300

    inicio da vitrine, service product criado

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { CarouselModule } from 'ngx-bootstrap/carousel';
-
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -11,6 +12,9 @@ import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { ShowcaseComponent } from './home/showcase/showcase.component';
 import { BannerHomeComponent } from './home/banner-home/banner-home.component';
+
+//services
+import { ProductService } from './product/product.service';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,8 @@ import { BannerHomeComponent } from './home/banner-home/banner-home.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     CarouselModule.forRoot()
   ],
   providers: [],
---
+++ my-app/src/app/product/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    id: number;
+    nome: string;
+    imagem: string;
+    descricao: string;
+    especificacao: string;
+    valorAtual: number;
+    valorAntigo: number;
+    porcentagemDesconto: number;
+    produtoEmDestaque : boolean;
+}
