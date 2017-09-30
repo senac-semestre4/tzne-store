5f2701561f438ecbeaa806fea8317a16bc1fc5af
Everton Roberto <everton_roliveira@outlook.com>
2017-09-30 10:40:22 -0300

    rodapé finalizado, newsletter finalizada

--- my-app/src/app/home/showcase/showcase.component.ts
+++ my-app/src/app/home/showcase/showcase.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ProductService } from "../../product/product.service";
+import { ProductService } from "../../services/product.service";
 
 @Component({
   selector: 'app-showcase',
@@ -9,6 +9,23 @@ import { ProductService } from "../../product/product.service";
   providers: [ProductService]
 })
 export class ShowcaseComponent{
+  private products;
+  private id: number;
+  private nome: string;
+  private imagem: string;
+  private descricao: string;
+  private especificacao: string;
+  private valorAtual: number;
+  private valorAntigo: number;
+  private porcentagemDesconto: number;
+  private produtoEmDestaque : boolean;
+
+  constructor(serviceProduct : ProductService) {
+    this.products = serviceProduct.getProdutosEmDestaque();
+    console.log("aqui",this.products);
+    //this.descricao = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos."
+    //this.especificacao = "É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. ";
+   }
   
   boxComprar : boolean = false;
 
@@ -18,5 +35,19 @@ export class ShowcaseComponent{
 
   ocultaBoxComprar(){
     this.boxComprar = false;
+  }
+
+  getProdutoEmDestaque(){
+    return [{
+      "id" : 1,
+      "nome" : "Camiseta Homem-Aranha",
+      "imagem" : "././assets/images/produtos/1.jpg",
+      "descricao" : "",
+      "especificacao" : "",
+      "valorAtual" : 59.41,
+      "valorAntigo" : 69.90,
+      "porcentagemDesconto" : 15,
+      "emDestaque" : true
+    }]
   }
 }
