dfe924a4deb178d893617c14784d449c696b7be0
AugustoAlencar <augusto.alc1@hotmail.com>
2017-09-30 14:27:43 -0300

    produtos vinculado com o serviço

--- my-app/src/app/home/showcase/showcase.component.ts
+++ my-app/src/app/home/showcase/showcase.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+//service
+import { ProductService } from '../../services/product.service';
 
-import { ProductService } from "../../services/product.service";
 
 @Component({
   selector: 'app-showcase',
   templateUrl: './showcase.component.html',
   styleUrls: ['./showcase.component.scss'],
-  providers: [ProductService]
 })
-export class ShowcaseComponent{
-  private products;
-  private id: number;
+export class ShowcaseComponent implements OnInit {
+
+
+ /*  private id: number;
   private nome: string;
   private imagem: string;
   private descricao: string;
@@ -18,36 +19,25 @@ export class ShowcaseComponent{
   private valorAtual: number;
   private valorAntigo: number;
   private porcentagemDesconto: number;
-  private produtoEmDestaque : boolean;
+  private produtoEmDestaque: boolean;*/
+  private products: any;
+  private boxComprar: boolean = false;
 
-  constructor(serviceProduct : ProductService) {
-    this.products = serviceProduct.getProdutosEmDestaque();
-    console.log("aqui",this.products);
-    //this.descricao = "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos."
-    //this.especificacao = "É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. ";
-   }
-  
-  boxComprar : boolean = false;
 
-  mostraBoxComprar(event){
+  constructor(private produtos: ProductService) {
+  }
+
+  ngOnInit() {
+    this.products = this.produtos.getProdutosEmDestaque();
+  }
+
+
+  mostraBoxComprar(event) {
     this.boxComprar = true;
   }
 
-  ocultaBoxComprar(){
+  ocultaBoxComprar() {
     this.boxComprar = false;
   }
 
-  getProdutoEmDestaque(){
-    return [{
-      "id" : 1,
-      "nome" : "Camiseta Homem-Aranha",
-      "imagem" : "././assets/images/produtos/1.jpg",
-      "descricao" : "",
-      "especificacao" : "",
-      "valorAtual" : 59.41,
-      "valorAntigo" : 69.90,
-      "porcentagemDesconto" : 15,
-      "emDestaque" : true
-    }]
-  }
 }
--- my-app/src/app/services/product.service.ts
+++ my-app/src/app/services/product.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductService {
 
+  constructor(){ }
+
   private produtosEmDestaque = [
     {
       "id" : 1,
@@ -10,8 +12,8 @@ export class ProductService {
       "imagem" : "././assets/images/produtos/1.jpg",
       "descricao" : "",
       "especificacao" : "",
-      "valorAtual" : 59.41,
-      "valorAntigo" : 69.90,
+      "valorAtual" : 59.00,
+      "valorAntigo" : 69.00,
       "porcentagemDesconto" : 15,
       "emDestaque" : true
     },
@@ -21,8 +23,8 @@ export class ProductService {
       "imagem" : "././assets/images/produtos/2.jpg",
       "descricao" : "",
       "especificacao" : "",
-      "valorAtual" : 69.90,
-      "valorAntigo" : 69.90,
+      "valorAtual" : 69.00,
+      "valorAntigo" : 69.00,
       "porcentagemDesconto" : 0,
       "emDestaque" : true
     },
@@ -44,11 +46,55 @@ export class ProductService {
       "descricao" : "",
       "especificacao" : "",
       "valorAtual" : 65.00,
-      "valorAntigo" : 56.52,
+      "valorAntigo" : 56.00,
+      "porcentagemDesconto" : 15,
+      "emDestaque" : true
+    },
+    {
+      "id" : 5,
+      "nome" : "Camiseta Homem-Aranha",
+      "imagem" : "././assets/images/produtos/1.jpg",
+      "descricao" : "",
+      "especificacao" : "",
+      "valorAtual" : 59.00,
+      "valorAntigo" : 69.00,
+      "porcentagemDesconto" : 15,
+      "emDestaque" : true
+    },
+    {
+      "id" : 6,
+      "nome" : "Camiseta Capitão América",
+      "imagem" : "././assets/images/produtos/2.jpg",
+      "descricao" : "",
+      "especificacao" : "",
+      "valorAtual" : 69.00,
+      "valorAntigo" : 69.00,
+      "porcentagemDesconto" : 0,
+      "emDestaque" : true
+    },
+    {
+      "id" : 7,
+      "nome" : "Camiseta manga longa Batman",
+      "imagem" : "././assets/images/produtos/3.jpg",
+      "descricao" : "",
+      "especificacao" : "",
+      "valorAtual" : 100.00,
+      "valorAntigo" : 89.00,
+      "porcentagemDesconto" : 11,
+      "emDestaque" : true
+    },
+    {
+      "id" : 8,
+      "nome" : "Camiseta Super-Homem",
+      "imagem" : "././assets/images/produtos/2.jpg",
+      "descricao" : "",
+      "especificacao" : "",
+      "valorAtual" : 65.00,
+      "valorAntigo" : 56.00,
       "porcentagemDesconto" : 15,
       "emDestaque" : true
     }
-  ]
+  ];
 
   private produtosVitrine = [
     {
@@ -57,8 +103,8 @@ export class ProductService {
       "imagem" : "././assets/images/produtos/5.jpg",
       "descricao" : "",
       "especificacao" : "",
-      "valorAtual" : 59.41,
-      "valorAntigo" : 69.90,
+      "valorAtual" : 59.00,
+      "valorAntigo" : 69.00,
       "porcentagemDesconto" : 15,
       "emDestaque" : false
     },
@@ -68,8 +114,8 @@ export class ProductService {
       "imagem" : "././assets/images/produtos/6.jpg",
       "descricao" : "",
       "especificacao" : "",
-      "valorAtual" : 69.90,
-      "valorAntigo" : 69.90,
+      "valorAtual" : 69.00,
+      "valorAntigo" : 69.00,
       "porcentagemDesconto" : 0,
       "emDestaque" : false
     },
@@ -91,14 +137,14 @@ export class ProductService {
       "descricao" : "",
       "especificacao" : "",
       "valorAtual" : 65.00,
-      "valorAntigo" : 56.52,
+      "valorAntigo" : 56.00,
       "porcentagemDesconto" : 15,
       "emDestaque" : false
     }
-  ]
+  ];
 
 
-  getProdutosEmDestaque(){
-    return this.getProdutosEmDestaque;
+  public getProdutosEmDestaque(){
+    return this.produtosEmDestaque;
   }
 }
