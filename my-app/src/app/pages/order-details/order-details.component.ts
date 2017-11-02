7710698343af40c22c73b922042a63134ae080a2
Everton Roberto <everton_roliveira@outlook.com>
2017-11-02 21:09:27 -0200

    finalizado layout tela order-details

--- my-app/src/app/pages/order-details/order-details.component.ts
+++ my-app/src/app/pages/order-details/order-details.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.component.scss']
 })
 export class OrderDetailsComponent implements OnInit {
-
+  private formaEntrega : string = "";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selecionaEntrega(event){
+    if(event.target.tagName == 'A'){
+      event.target.childNodes[1].checked = true;
+    }else{
+      event.target.parentNode.children["0"].checked = true;
+    }
   }
 
 }
